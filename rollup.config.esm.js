4064d8972151ab9b0d584900a1be64e61f872d07
i18u <iahu1988@gmail.com>
2021-08-24 15:18:30 +0800

    script: clear build output

--- rollup.config.esm.js
+++ rollup.config.esm.js
@@ -2,13 +2,20 @@ import multiInput from 'rollup-plugin-multi-input'
 import importCss from 'rollup-plugin-lit-css'
 import typescript from 'rollup-plugin-typescript2'
 
+const input = ['./src/**/*.ts', '!./src/**/*.stories.ts']
+
+if (process.env.NODE_ENV !== 'test') {
+  input.push('!./src/**/*.test.ts')
+}
+const exclude = input.filter(s => s.startsWith('!')).map(s => s.slice(1))
+
 /**
  * @type {import('rollup').RollupOptions}
  */
 export default {
-  input: ['./src/**/*.ts', '!./src/**/*.stories.ts'],
+  input,
   output: { dir: './dist/esm', format: 'esm' },
-  plugins: [multiInput(), typescript(), importCss()],
+  plugins: [multiInput(), typescript({ tsconfig: './tsconfig.json', tsconfigOverride: { exclude } }), importCss()],
   external: [
     'lit',
     'lit/decorators.js',
